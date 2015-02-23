c4492b2ffba3159330a6cf799e8388ad4aa30116
assem-el-merhi <Assem@Assems-MacBook-Pro.local>
2015-02-23 18:59:00 -0500

    fixed

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ var Red = {
 
 var berries2 = [Blue, Grape, Red];
 
-// console.log( dinos2[1].age );
+// console.log( berries2[1].age );
 
 berries2.forEach(function (item) {
   document.write('<dl>'); 
@@ -54,8 +54,32 @@ berries2.forEach(function (item) {
   document.write('</dl>');
 });
 
-writeberries2( [Blue, Grape] );
-writeberries2( [Grape, Red] );
-writeberries2( [Red, Blue] );
+var writeberries2 = function (allberries2) {
+    allberries2.forEach(function (item) {
+  document.write('<dl>'); 
+  
+      document.write('<dt>Fruit:</dt>')
+  document.write('<dd>' + item.Fruit + '</dd>');
+  
+  document.write('<dt>Plant:</dt>')
+  document.write('<dd>' + item.plant + '</dd>');
+    
+  document.write('<dt>GrowsOnSrub:</dt>')
+  document.write('<dd>' + item.growsOnSrub + '</dd>');
+    
+  document.write('<dt>Energy:</dt>')
+  document.write('<dd>' + item.energy + '</dd>');
+    
+  document.write('<dt>Carbohydrates:</dt>')
+  document.write('<dd>' + item.carbohydrates + '</dd>');
+    
+  document.write('<dt>Protein:</dt>')
+  document.write('<dd>' + item.protein + '</dd>');
+
+  
+  document.write('</dl>');
+    });
+};
+
 
 
